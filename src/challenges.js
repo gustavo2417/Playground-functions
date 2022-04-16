8fc53a0b91fc799a31f9c313d409a203964d74db
Gustavo <gustavodarocha241@gmail.com>
2022-04-16 17:15:48 -0300

    conclusao exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,11 +15,13 @@ let calculo = 0;
 return calculo = base*height/2
 }
 console.log(calcArea(51,1))
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
 
+// Desafio 3
+function splitSentence(phrase) {
+  let strings = phrase.split(" ");
+  return strings
+}
+console.log(splitSentence("foguete"))
 // Desafio 4
 function concatName() {
   // seu código aqui
