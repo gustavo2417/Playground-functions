085f752588e93c212255cb73bac37647012f49a6
Gustavo <gustavodarocha241@gmail.com>
2022-04-19 18:48:43 -0300

    completa desafios 4,5,6,8

--- src/challenges.js
+++ src/challenges.js
@@ -22,31 +22,60 @@ function splitSentence(phrase) {
   return strings
 }
 console.log(splitSentence("foguete"))
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
 
+// Desafio 4
+function concatName(array) {
+  let result = [];
+  let last = array[array.length -1];
+  let first = array[0];
+return result = last + ", " + first
+}
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+   let vitorias = wins * 3;
+   let empates = ties;
+   return footballPoints = vitorias + empates;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
-
+function highestCount(numbers) {
+  let count = 0;
+  let maxNumer = Math.max(...numbers)
+  for (let index = 0; index < numbers.length; index ++) {
+    if (maxNumer === numbers[index]) {
+      count ++
+    }
+   }
+   return count
+  }
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  if (mouse < cat1 > cat2){
+    return 'cat2'
+}else if (mouse < cat2 > cat1) {
+    return 'cat1'
+}  else {
+  return 'os gatos trombam e o rato foge'
+  }
 }
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(primos) {
+  let array = [];
+  for (let i = 0; i < primos.length;i += 1) {
+    if ((primos[i] % 3 === 0) && (primos[i] % 5 === 0)){
+    array.push("fizzBuzz")
+   }else if((primos[i] % 3)  === 0) {
+      array.push("fizz")
+    }else if ((primos[i] % 5) === 0) {
+      array.push("buzz")
+    }else {
+      array.push("bug!")
+    }
+  }
+  return array
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
@@ -57,7 +86,7 @@ function decode() {
 
 // Desafio 10
 function techList() {
-  // seu código aqui
+  
 }
 
 module.exports = {
